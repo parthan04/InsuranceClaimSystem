45666e20190f5079c0f28446ab59f248d916a853
Arjun Aju <arjunanu6666@gmail.com>
2026-04-03 14:51:32 +0530

    api changes

--- frontend-app/src/services/api.tsx
+++ frontend-app/src/services/api.tsx
@@ -1,9 +1,10 @@
 import axios from "axios";
 
-const BASE_URL = "http://127.0.0.1:8000/api/";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 const api = axios.create({
   baseURL: BASE_URL,
+  timeout: 10000,
 });
 
 // 🔐 Attach access token
